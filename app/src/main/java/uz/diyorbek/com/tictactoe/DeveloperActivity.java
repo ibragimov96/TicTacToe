762bdcb48d55de1040481eb504ef40d4a2838b94
ibragimov96 <ibragimoff.diyorbek@gmail.com>
2020-05-21 16:42:41 +0500

    Initial commit

---
+++ app/src/main/java/uz/diyorbek/com/tictactoe/DeveloperActivity.java
@@ -0,0 +1,17 @@
+package uz.diyorbek.com.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class DeveloperActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_developer);
+
+        Toast.makeText(this, R.string.about, Toast.LENGTH_SHORT).show();
+    }
+}
